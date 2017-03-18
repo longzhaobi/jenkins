6876ddceb5f59d7ededcce37b2846e3c35837e08
龙召碧 <714037058@qq.com>
2017-03-18 21:15:20 +0800

    改动测试持续集成

--- src/main/java/com/loozb/web/WebController.java
+++ src/main/java/com/loozb/web/WebController.java
@@ -10,7 +10,7 @@ public class WebController {
 	@RequestMapping("index")
 	public ModelAndView index() {
 		ModelAndView mav = new ModelAndView("index");
-		mav.addObject("content", "这里是测试jenkins持续集成的DEMO，可以改动此处进行测试11");
+		mav.addObject("content", "这里是测试jenkins持续集成的DEMO，可以改动此处进行测试1122222");
 		return mav;
 	}
 
